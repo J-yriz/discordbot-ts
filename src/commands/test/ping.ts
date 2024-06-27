da28f5eef6ebb24362f767b06b65f06e1ad365fe
Jyriz <discordketukangan123@gmail.com>
2024-06-27 22:39:23 +0700

    feat: change test feature

--- src/commands/test/ping.ts
+++ src/commands/test/ping.ts
@@ -1,19 +1,18 @@
-import App from '../../utils/discordBot';
-import { ChatInputCommandInteraction, SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } from 'discord.js';
+import App from "../../utils/discordBot";
+import { ChatInputCommandInteraction, SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } from "discord.js";
 
 const ping = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with ping!')
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with ping!")
         .setDMPermission(false)
         .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
-	async exec(interaction: ChatInputCommandInteraction, app: App) {
-		await interaction.reply({ embeds: [
-			new EmbedBuilder()
-				.setTitle(`${app.ws.ping}ms`)
-				.setTimestamp()
-		], ephemeral: true});
-	},
+    async exec(interaction: ChatInputCommandInteraction, app: App) {
+        await interaction.reply({
+            embeds: [new EmbedBuilder().setTitle(`${app.ws.ping}ms`).setTimestamp()],
+            ephemeral: true,
+        });
+    },
 };
 
 export default ping;
