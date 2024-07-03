44373d78ecca0aa5d9bd01ebde5b99cc00814b12
Jyriz <discordketukangan123@gmail.com>
2024-07-03 22:05:32 +0700

    refactor: delete return selector

--- src/api/lavalink/trackGet.ts
+++ src/api/lavalink/trackGet.ts
@@ -1,38 +1,13 @@
 import axios from "axios";
 import config, { lavalink } from "../../config";
-
-interface IDataResponse {
-    data: {
-        loadType: string;
-        tracks: [
-            {
-                encode: string;
-                track: string;
-                info: {
-                    identifier: string;
-                    isSeekable: boolean;
-                    author: string;
-                    length: number;
-                    isStream: boolean;
-                    position: number;
-                    title: string;
-                    uri: string;
-                    sourceName: string;
-                };
-            }
-        ];
-    };
-}
+import { IDataResponse } from "../../utils/interface";
 
 const trackGet = async (dataMusic: string): Promise<string | {}> => {
     const trackResponse: IDataResponse = await axios.get(`${lavalink("http")}/v3/loadtracks`, {
         headers: { Authorization: config.Lavalink.LavaPass },
         params: { identifier: `ytsearch: ${dataMusic}` },
     });
-    if (!trackResponse.data.tracks.length) {
-        return "No tracks found";
-    }
-    return trackResponse.data.tracks[0].info;
+    return trackResponse.data.tracks;
 };
 
 export default trackGet;
